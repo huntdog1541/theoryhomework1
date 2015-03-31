8a3611980a8b37ec5669cb298ac92d4f33ec51a9
David Hunt <huntdog1541@gmail.com>
2015-03-31 16:44:49 -0500

    update

--- Node.java
+++ Node.java
@@ -6,7 +6,7 @@ public class Node(){
 
         }
 
-        public static void Main(String argc)
+        public static void main(String[] args)
         {
             System.out.println("Hello, world!");
         }
